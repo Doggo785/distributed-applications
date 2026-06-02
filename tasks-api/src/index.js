b5f284a197a4340c7ad84a42d3ed2fd549d45eb5
Doggo785 <stephane.plathey@proton.me>
2026-06-02 12:14:08 +0200

    feat: implement getAllTasks endpoint

--- tasks-api/src/index.js
+++ tasks-api/src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const tasksRoutes = require('./routes/tasks.routes');
 const app = express();
 const port = 3000;
 
@@ -16,6 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (request, response) => {
   response.send('Hello World!');
 });
+
+app.use('/api/tasks', tasksRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
