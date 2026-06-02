e1c49243176f4f2af5e990b013faef44ff0c6eae
Doggo785 <stephane.plathey@proton.me>
2026-06-02 10:05:17 +0200

    feat: configure MongoDB connection with env variables

--- tasks-api/src/index.js
+++ tasks-api/src/index.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+const { MONGO_HOST, MONGO_PORT, MONGO_DB } = process.env;
+
+mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('Error connecting to MongoDB:', error));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (request, response) => {
   response.send('Hello World!');
